bb9b8ba282b170a0900ac65b88860ae7c42c5f25
flyplay91 <flyplay91@gmail.com>
2021-04-21 01:01:41 -0400

    changed Read.me & included bootstrap js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,14 +8,17 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>Dahsboard</title>
+
     <!-- Styles -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    
-    <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
 
+    <!-- Script -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
 </head>
+
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
